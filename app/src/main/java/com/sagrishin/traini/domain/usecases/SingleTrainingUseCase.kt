e4bc3d3a220f96708ea6febd049496b4eaa6cb64
Sergey <sergey.grishin.dev@gmail.com>
2021-08-17 11:52:02 +0300

    Optimize selection of one training from database

--- app/src/main/java/com/sagrishin/traini/domain/usecases/SingleTrainingUseCase.kt
+++ app/src/main/java/com/sagrishin/traini/domain/usecases/SingleTrainingUseCase.kt
@@ -1,16 +1,14 @@
 package com.sagrishin.traini.domain.usecases
 
-import com.sagrishin.common.utils.zipFlows
+import com.sagrishin.common.utils.toMap
 import com.sagrishin.traini.domain.converters.toUiModel
 import com.sagrishin.traini.domain.repositories.ExerciseRepetitionsRepository
 import com.sagrishin.traini.domain.repositories.ExercisesRepository
 import com.sagrishin.traini.domain.repositories.MusclesRepository
 import com.sagrishin.traini.domain.repositories.TrainingsRepository
 import com.sagrishin.traini.presentation.uimodels.*
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flatMapConcat
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
@@ -23,29 +21,25 @@ class SingleTrainingUseCase @Inject constructor(
 
     fun getTrainingWithExercisesAndRepetitionsBy(trainingId: Long): Flow<UiTrainingWithExercisesAndRepetitions> {
         return trainingsRepository.getTrainingExerciseLinksBy(trainingId).flatMapConcat { trainingExerciseLinks ->
-//            val exercises = exercisesRepository.getExercisesBy(trainingExerciseLinks.map { it.exerciseId })
-//            val muscles = musclesRepository.getMusclesBy(exercises.map { it.muscleId }).toMap { it.id to it }
-//
-//            val uiExercises = exercises.map { exercise ->
-//                val uiMuscle = muscles.getValue(exercise.muscleId).toUiModel()
-//                val uiExerciseData = UiExerciseData(exercise.id, exercise.name, uiMuscle)
-//            }
-
-
-            trainingExerciseLinks.map { trainingExerciseLink ->
-                val exercise = exercisesRepository.getExerciseBy(trainingExerciseLink.exerciseId)
-                val uiMuscle = musclesRepository.getMuscleBy(exercise.muscleId).toUiModel()
-                val uiExerciseData = UiExerciseData(exercise.id, exercise.name, uiMuscle)
-
-                repetitionsRepository.getExerciseRepetitionsBy(trainingExerciseLink.id).map { repetitions ->
-                    val uiRepetitions = repetitions.map { UiExerciseRepetition(it.id, it.weight, it.repetitionsCount) }
-                    UiExerciseWithRepetitions(uiExerciseData, uiRepetitions)
-                }.flowOn(Dispatchers.IO)
-            }.zipFlows()
-        }.map { exercises ->
             val training = trainingsRepository.getTrainingBy(trainingId)
             val uiTraining = UiTrainingData(training.id, training.startDateTime)
-            UiTrainingWithExercisesAndRepetitions(uiTraining, exercises)
+
+            val trainingLinksMap = trainingExerciseLinks.toMap { it.id to it }
+            val exercises = exercisesRepository.getExercisesBy(trainingExerciseLinks.map { it.exerciseId }).toMap { it.id to it }
+            val muscles = musclesRepository.getMusclesBy(exercises.values.map { it.muscleId }).toMap { it.id to it }
+
+            repetitionsRepository.getExerciseRepetitionsBy(trainingExerciseLinks.map { it.id }).map { repetitions ->
+                repetitions.map {
+                    val exercise = exercises.getValue(trainingLinksMap.getValue(it.trainingExerciseId).exerciseId)
+                    val uiMuscle = muscles.getValue(exercise.muscleId).toUiModel()
+                    val uiExerciseData = UiExerciseData(exercise.id, exercise.name, uiMuscle)
+                    val uiRepetitions = repetitions.map { UiExerciseRepetition(it.id, it.weight, it.repetitionsCount) }
+
+                    UiExerciseWithRepetitions(uiExerciseData, uiRepetitions)
+                }
+            }.map {
+                UiTrainingWithExercisesAndRepetitions(uiTraining, it)
+            }
         }
     }
 
